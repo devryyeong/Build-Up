2694ec88befc40243016478d5d7bd08376053553
SeonjaeHyeon <hsj106@mju.ac.kr>
2022-01-27 12:54:01 +0900

    add sms router & update auth, user router

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,4 +6,13 @@ router.get("/getUser", (req, res, next) => {
   console.log("HTTP GET: getUser");
 });
 
+router.post("/setInfo", (req, res, next) => {
+  const { info } = req.body;
+  let result = {};
+
+  console.log("HTTP POST: ");
+
+  return res.json(result);
+});
+
 module.exports = router;
